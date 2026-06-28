dee20e05f8b1f87aa7a6f5e65e990cd956fbb1eb
Kushal Berad <kushalberadwork@gmail.com>
2026-06-28 13:45:13 +0530

    Production deployment update

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Task Tracker Backend API Running");
+});
+
 // routes
 app.use("/api/tasks", require("./routes/taskroutes"));
 
